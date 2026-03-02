23d4b9731f8b9f32937707484d6f752a5138b418
JanSharp <steinmeister36@gmail.com>
2026-03-02 21:52:15 +0100

    Remove redundant pooling logic
    Should have checked docs first to find PlayOneShot sooner

--- Runtime/Scripts/UISoundsManager.cs
+++ Runtime/Scripts/UISoundsManager.cs
@@ -7,14 +7,6 @@ namespace JanSharp.Internal
     [CustomRaisedEventsDispatcher(typeof(UISoundsEventAttribute), typeof(UISoundsEventType))]
     public class UISoundsManager : UISoundsManagerAPI
     {
-        // [SerializeField] private UISoundsAudioSource[] allSources;
-        // private UISoundsAudioSource[] inactiveSources = new UISoundsAudioSource[ArrList.MinCapacity];
-        // private int inactiveSourcesCount = 0;
-        // private UISoundsAudioSource[] activeSources = new UISoundsAudioSource[ArrList.MinCapacity];
-        // private int activeSourcesCount = 0;
-
-        // private bool isInitialized = false;
-
         [SerializeField] private AudioSource audioSource;
 
         [SerializeField] private bool muted = false;
@@ -42,71 +34,12 @@ namespace JanSharp.Internal
             }
         }
 
-        // private void Start() => Initialize();
-
-        // private void Initialize()
-        // {
-        //     if (isInitialized)
-        //         return;
-        //     isInitialized = true;
-        //     ArrList.AddRange(ref inactiveSources, ref inactiveSourcesCount, allSources);
-        //     for (int i = 0; i < inactiveSourcesCount; i++)
-        //         inactiveSources[i].index = i;
-        // }
-
         public override void PlaySound(AudioClip audioClip, float volume)
         {
             if (muted)
                 return;
             audioSource.PlayOneShot(audioClip, volume);
-            // Initialize();
-            // UISoundsAudioSource source = GetSourceToPlaySoundWith();
-            // source.PlaySound(audioClip, volume);
         }
-
-        // private UISoundsAudioSource GetSourceToPlaySoundWith()
-        // {
-        //     if (inactiveSourcesCount != 0)
-        //         return inactiveSources[inactiveSourcesCount - 1];
-        //     float smallestRemainder = float.PositiveInfinity;
-        //     UISoundsAudioSource bestSource = null;
-        //     for (int i = activeSourcesCount - 1; i >= 0; i--)
-        //     {
-        //         UISoundsAudioSource source = activeSources[i];
-        //         float remainder = source.GetRemainingActiveTime();
-        //         if (remainder >= smallestRemainder)
-        //             continue;
-        //         smallestRemainder = remainder;
-        //         bestSource = source;
-        //     }
-        //     return bestSource;
-        // }
-
-        // public void BecomeActive(UISoundsAudioSource source)
-        // {
-        //     int index = source.index;
-        //     if (index != (--inactiveSourcesCount))
-        //     {
-        //         UISoundsAudioSource top = inactiveSources[inactiveSourcesCount];
-        //         inactiveSources[index] = top;
-        //         top.index = index;
-        //     }
-        //     source.index = activeSourcesCount;
-        //     ArrList.Add(ref activeSources, ref activeSourcesCount, source);
-        // }
-
-        // public void BecomeInactive(UISoundsAudioSource source)
-        // {
-        //     int index = source.index;
-        //     if (index != (--activeSourcesCount))
-        //     {
-        //         UISoundsAudioSource top = activeSources[activeSourcesCount];
-        //         activeSources[index] = top;
-        //         top.index = index;
-        //     }
-        //     source.index = inactiveSourcesCount;
-        //     ArrList.Add(ref inactiveSources, ref inactiveSourcesCount, source);
-        // }
 
         #region EventDispatcher
 
